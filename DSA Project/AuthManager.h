3b741960270d52f62003188d64d4a8127f8d10ad
Waleed Ahmed Razzaqi <146324731+WaleedAhmed10@users.noreply.github.com>
2025-05-18 17:43:22 +0500

    Update AuthManager.h

--- DSA Project/AuthManager.h
+++ DSA Project/AuthManager.h
@@ -1,11 +1,25 @@
 #pragma once
 #ifndef AUTHMANAGER_H
 #define AUTHMANAGER_H
+#include <string>
+using namespace std;
 
 class AuthManager
 {
+private:
+    struct Node
+    {
+        string password;
+        Node* next;
+    };
+
+    Node* head;
+
 public:
+    AuthManager();
+    ~AuthManager();
+    void addPassword(const string& newPassword);
     bool authenticate();
 };
 
-#endif 
+#endif
