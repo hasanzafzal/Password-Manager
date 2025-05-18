c36bdda1d13bddef70bcedbb1bab2571d41c6fe5
Waleed Ahmed Razzaqi <146324731+WaleedAhmed10@users.noreply.github.com>
2025-05-18 17:42:48 +0500

    Update AuthManager.cpp

--- DSA Project/AuthManager.cpp
+++ DSA Project/AuthManager.cpp
@@ -1,15 +1,44 @@
 #include "AuthManager.h"
 #include <iostream>
-#include <string>
 using namespace std;
+
+AuthManager::AuthManager()
+{
+    head = nullptr;
+    addPassword("admin");
+    addPassword("root");
+    addPassword("1234"); addPassword("0000");
+}
+
+AuthManager::~AuthManager()
+{
+    Node* current = head;
+    while (current != nullptr)
+    {
+        Node* toDelete = current;
+        current = current->next;
+        delete toDelete;
+    }
+}
+
+void AuthManager::addPassword(const string& newPassword)
+{
+    Node* newNode = new Node{ newPassword, head };
+    head = newNode;
+}
 
 bool AuthManager::authenticate()
 {
     string input;
-    const string masterPassword = "admin";
-
     cout << "Enter master password: ";
     cin >> input;
 
-    return input == masterPassword;
+    Node* current = head;
+    while (current != nullptr)
+    {
+        if (current->password == input)
+            return true;
+        current = current->next;
+    }
+    return false;
 }
