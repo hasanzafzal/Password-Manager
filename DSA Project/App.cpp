6bc7e9482c163f69d7297a7d3a6f9903db454496
Hasan Zahid <hasanzafzal@gmail.com>
2025-05-27 17:46:12 +0500

    Update App.cpp

--- DSA Project/App.cpp
+++ DSA Project/App.cpp
@@ -15,32 +15,32 @@ void App::run()
     {
         cout << "Authentication failed. Exiting...\n";
         return;
-
-        CredentialManager manager;
-        int choice;
-        do
-        {
-            cout << "\n=== Password Manager ===\n";
-            cout << "1. Add Credential\n";
-            cout << "2. View Credentials\n";
-            cout << "0. Exit\n";
-            cout << "Enter your choice: ";
-            cin >> choice;
-
-            switch (choice)
-            {
-            case 1:
-                manager.addCredential();
-                break;
-            case 2:
-                manager.viewCredentials();
-                break;
-            case 0:
-                cout << "Exiting...\n";
-                break;
-            default:
-                cout << "Invalid choice.\n";
-            }
-        } while (choice != 0);
     }
+
+    CredentialManager manager;
+    int choice;
+    do
+    {
+        cout << "\n=== Password Manager ===\n";
+        cout << "1. Add Credential\n";
+        cout << "2. View Credentials\n";
+        cout << "0. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            manager.addCredential();
+            break;
+        case 2:
+            manager.viewCredentials();
+            break;
+        case 0:
+            cout << "Exiting...\n";
+            break;
+        default:
+            cout << "Invalid choice.\n";
+        }
+    } while (choice != 0);
 }
